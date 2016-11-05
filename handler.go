542f9aa324bd3d0f3ad6c5eb691f25121408cfbe
Rizwan Tejpar <rtejpar@gmail.com>
2016-11-05 12:22:55 -0700

    moved core logic into validate

--- handler.go
+++ handler.go
@@ -12,73 +12,79 @@ func defaultFailureHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "", http.StatusBadRequest)
 }
 
+// Handler for middleware
 func (m *Middleware) Handler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		r = addNosurfContext(r)
 		defer ctxClear(r)
 		w.Header().Add("Vary", "Cookie")
 
-		var realToken []byte
-
-		tokenCookie, err := r.Cookie(m.Options.baseCookie.Name)
-		if err == nil {
-			realToken = b64decode(tokenCookie.Value)
-		}
-
-		// If the length of the real token isn't what it should be,
-		// it has either been tampered with,
-		// or we're migrating onto a new algorithm for generating tokens,
-		// or it hasn't ever been set so far.
-		// In any case of those, we should regenerate it.
-		//
-		// As a consequence, CSRF check will fail when comparing the tokens later on,
-		// so we don't have to fail it just yet.
-		if len(realToken) != m.Options.TokenLength {
-			m.RegenerateToken(w, r)
-		} else {
-			ctxSetToken(r, realToken, m.Options.TokenLength)
-		}
-
-		if sContains(m.Options.SafeMethods, r.Method) {
-			// short-circuit with a success for safe methods
-			h.ServeHTTP(w, r)
-			return
-		}
-
-		// if the request is secure, we enforce origin check
-		// for referer to prevent MITM of http->https requests
-		if r.URL.Scheme == "https" {
-			referer, err := url.Parse(r.Header.Get("Referer"))
-
-			// if we can't parse the referer or it's empty,
-			// we assume it's not specified
-			if err != nil || referer.String() == "" {
-				ctxSetReason(r, ErrNoReferer)
-				m.Options.failureHandler.ServeHTTP(w, r)
-				return
-			}
-
-			// if the referer doesn't share origin with the request URL,
-			// we have another error for that
-			if !sameOrigin(referer, r.URL) {
-				ctxSetReason(r, ErrBadReferer)
-				m.Options.failureHandler.ServeHTTP(w, r)
-				return
-			}
-		}
-
-		// Finally, we check the token itself.
-		sentToken := extractToken(r, m.Options.HeaderName, m.Options.FormFieldName)
-
-		if !verifyToken(realToken, sentToken, m.Options.TokenLength) {
-			ctxSetReason(r, ErrBadToken)
+		if err := m.Validate(w, r); err != nil {
 			m.Options.failureHandler.ServeHTTP(w, r)
 			return
 		}
 
 		h.ServeHTTP(w, r)
 	})
+}
+
+func (m *Middleware) Validate(w http.ResponseWriter, r *http.Request) error {
+
+	var realToken []byte
+
+	tokenCookie, err := r.Cookie(m.Options.baseCookie.Name)
+	if err == nil {
+		realToken = b64decode(tokenCookie.Value)
+	}
+
+	// If the length of the real token isn't what it should be,
+	// it has either been tampered with,
+	// or we're migrating onto a new algorithm for generating tokens,
+	// or it hasn't ever been set so far.
+	// In any case of those, we should regenerate it.
+	//
+	// As a consequence, CSRF check will fail when comparing the tokens later on,
+	// so we don't have to fail it just yet.
+	if len(realToken) != m.Options.TokenLength {
+		m.RegenerateToken(w, r)
+	} else {
+		ctxSetToken(r, realToken, m.Options.TokenLength)
+	}
+
+	if sContains(m.Options.SafeMethods, r.Method) {
+		// short-circuit with a success for safe methods
+		return nil
+	}
+
+	// if the request is secure, we enforce origin check
+	// for referer to prevent MITM of http->https requests
+	if r.URL.Scheme == "https" {
+		referer, err := url.Parse(r.Header.Get("Referer"))
+
+		// if we can't parse the referer or it's empty,
+		// we assume it's not specified
+		if err != nil || referer.String() == "" {
+			ctxSetReason(r, ErrNoReferer)
+			return ErrNoReferer
+		}
+
+		// if the referer doesn't share origin with the request URL,
+		// we have another error for that
+		if !sameOrigin(referer, r.URL) {
+			ctxSetReason(r, ErrBadReferer)
+			return ErrBadReferer
+		}
+	}
+
+	// Finally, we check the token itself.
+	sentToken := extractToken(r, m.Options.HeaderName, m.Options.FormFieldName)
+
+	if !verifyToken(realToken, sentToken, m.Options.TokenLength) {
+		ctxSetReason(r, ErrBadToken)
+		return ErrBadToken
+	}
+
+	return nil
 }
 
 // Generates a new token, sets it on the given request and returns it
